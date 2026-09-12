1703ecb7f12356586f0493b2714207a1c2158baf
Zaid Rouf Shah <zaidroufshah@gmail.com>
2026-09-12 18:54:31 +0530

    Add GET endpoint for sample transactions

--- backend/main.py
+++ backend/main.py
@@ -3,5 +3,26 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-    return {"message": "Personal Finance API is running"}
+
+def read_routes():
+    return {"message": "My app is running"}
+
+
+
+transactions = [ {"id": 1,
+    "title": "Lunch",
+    "amount": 120,
+    "type": "expense"
+},
+{"id": 2,
+    "title": "salary",
+    "amount": 20000,
+    "type": "income"
+}
+
+
+]
+
+@app.get("/transactions")
+def read_transactions():
+    return {"transactions": transactions}
